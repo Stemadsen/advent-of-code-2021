58f26eab85aeb1e8711e2d4f536d335a59604517
stemadsen <stemadsen@gmail.com>
2021-12-15 21:34:58 +0100

    Solution for puzzle 14, part 2

--- 14/part 2/solution.py
+++ 14/part 2/solution.py
@@ -1,5 +1,5 @@
 input_template = 'FNFPPNKPPHSOKFFHOFOC'
-input_rules = {
+input_rules: dict[str, str] = {
     'VS': 'B',
     'SV': 'C',
     'PP': 'N',
@@ -103,26 +103,47 @@ input_rules = {
 }
 
 
-def apply_rules(string: str):
-    length = len(string)
-    for i in range(1, length):
-        pair_start = length - 1 - i  # work backwards through the string
-        pair = string[pair_start:pair_start + 2]
-        insertion = input_rules[pair]
-        if insertion:
-            string = string[0:pair_start + 1] + insertion + string[pair_start + 1:]
-    return string
+def fill_counts_dict(strings, counts: dict):
+    for string in strings:
+        increase_count(string, counts, 1)
 
 
-string = input_template
-for i in range(0, 10):
-    string = apply_rules(string)
+def increase_count(string: str, counts: dict, amount: int):
+    if string in counts.keys():
+        counts[string] += amount
+    else:
+        counts[string] = amount
 
-histogram = {}
-for i in range(0, len(string)):
-    char = string[i]
-    if not histogram.get(char):
-        histogram[char] = 0
-    histogram[char] += 1
 
-print(max(histogram.values()) - min(histogram.values()))
+def decrease_count(string: str, counts: dict, amount: int):
+    if string in counts.keys():
+        counts[string] -= amount
+
+
+def apply_rules(letter_counts: dict[str, int], pair_counts: dict[str, int]):
+    letter_counts_copy = letter_counts.copy()
+    pair_counts_copy = pair_counts.copy()
+    for pair, pair_count in pair_counts.items():
+        insertion_letter = input_rules[pair]
+        if insertion_letter:
+            increase_count(insertion_letter, letter_counts_copy, pair_count)
+            increase_count(pair[0] + insertion_letter, pair_counts_copy, pair_count)
+            increase_count(insertion_letter + pair[1], pair_counts_copy, pair_count)
+            decrease_count(pair, pair_counts_copy, pair_count)
+    return letter_counts_copy, pair_counts_copy
+
+
+def solve():
+    letter_counts: dict[str, int] = {}
+    pair_counts: dict[str, int] = {}
+
+    fill_counts_dict(input_template, letter_counts)
+    fill_counts_dict([input_template[i:i + 2] for i in range(0, len(input_template) - 1)], pair_counts)
+
+    for i in range(0, 40):
+        letter_counts, pair_counts = apply_rules(letter_counts, pair_counts)
+
+    return max(letter_counts.values()) - min(letter_counts.values())
+
+
+print(solve())
